46c076c25b0f6156399f2caac100adf566e3ff02
spidykehde <leoff071003@gmail.com>
2026-06-05 09:44:55 +0530

    Improve sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,32 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl =
+    "https://sneh-portfolio-pi.vercel.app";
+
   return [
     {
-      url: "https://sneh-portfolio-pi.vercel.app",
+      url: baseUrl,
       lastModified: new Date(),
       priority: 1,
+    },
+
+    {
+      url: `${baseUrl}/#projects`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/#services`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/#about`,
+      lastModified: new Date(),
+      priority: 0.8,
     },
   ];
 }
